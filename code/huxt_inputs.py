9f11e4970f9a7cc5f2f29213d73f5ceedf409f4c
mjrutala <mrutala@bu.edu>
2023-10-23 16:54:18 +0100

    Added first pass attempt at calculating reference R, MJD, Carr. Lon.; Logic behind these should probably be double-checked

--- code/huxt_inputs.py
+++ code/huxt_inputs.py
@@ -1302,7 +1302,7 @@ def generate_vCarr_from_OMNI(runstart, runend, nlon_grid=128, dt=1*u.day,
         return time_grid[mask], vgrid_carr_recon_forward[:, mask], bgrid_carr_recon_forward[:, mask]
 
 def generate_vCarr_from_VSWUM(runstart, runend, nlon_grid=128, dt=1*u.day, 
-                             ref_r = 215*u.solRad, corot_type = 'both'):
+                             ref_r = 329*u.solRad, corot_type = 'both'):
     """
     A function to read in VSWUM (Virtual Solar Wind Upstream Monitor) 
     data and generate: 
@@ -1337,6 +1337,7 @@ def generate_vCarr_from_VSWUM(runstart, runend, nlon_grid=128, dt=1*u.day,
     
     vswum = pd.read_csv(filepath)
     data = vswum[cols_to_use].copy()
+    data.rename(columns={'mu_v_mag_SW':'V'}, inplace=True)
     
     #   MJR 20231023: Shouldn't be needed if there's no bad data in VSWUM,,,
     # # Set invalid data points to NaN
@@ -1410,22 +1411,29 @@ def generate_vCarr_from_VSWUM(runstart, runend, nlon_grid=128, dt=1*u.day,
     mask_data_int['R'] = np.sqrt(np.sum(xyz_coords**2, axis=1)) * u.km
     
     spice.kclear()
-    return pos
     
     #map each point back/forward to the reference radial distance
-    omni_int['mjd_ref'] = omni_int['mjd']
-    omni_int['Carr_lon_ref'] = omni_int['Carr_lon_unwrap']
-    for t in range(0, len(omni_int)):
+    mask_data_int['mjd_ref'] = mask_data_int['mjd']
+    mask_data_int['Carr_lon_ref'] = mask_data_int['Carr_lon_unwrap']
+    #for t in range(0, len(mask_data_int)):
+    for t, row in mask_data_int.iterrows():
         #time lag to reference radius
-        delta_r = (ref_r.to(u.km) - omni_int['R'][t]).value
-        delta_t = delta_r/omni_int['V'][t]/24/60/60
-        omni_int['mjd_ref'][t] = omni_int['mjd_ref'][t]  + delta_t
+        delta_r = (ref_r.to(u.km) - row['R']*u.km).value  #  MJR: Don't know why *u.km needs to be added...
+        delta_t = delta_r/row['V']/24/60/60
+        mask_data_int.at[t, 'mjd_ref'] = row['mjd_ref'] + delta_t
         #change in Carr long of the measurement
-        omni_int['Carr_lon_ref'][t] =  omni_int['Carr_lon_ref'][t] - \
+        
+        #   MJR 20231023: !!!! Need to think about this more carefully
+        #   Reference Carrington Longitude is already calculated taking light travel time into account
+        #   Here, we're taking plasma travel time into account (effectively) (I think)
+        mask_data_int.at[t, 'Carr_lon_ref'] = row['Carr_lon_ref'] - \
             delta_t *daysec * 2 * np.pi /synodic_period
     
     #  Fix R to non-astropy, unitless values for compatibility
-    omni_int['R'] = omni_int['R'].to_numpy('float64')
+    mask_data_int['R'] = mask_data_int['R'].to_numpy('float64')
+    
+    
+    return mask_data_int
     
     #sort the omni data by Carr_lon_ref for interpolation
     omni_temp = omni_int.copy()
